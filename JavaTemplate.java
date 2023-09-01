ee6bc8232400e89118cec984c85380a0bf6c9a2e
Badal Soni <111721917+BadalSoni03@users.noreply.github.com>
2023-09-01 12:01:36 +0530

    Update

--- JavaTemplate.java
+++ JavaTemplate.java
@@ -1,12 +1,49 @@
 import java.util.*;
 import java.io.*;
 
-public class ContestSolution {
+// For codechef remove public 
+public class MainClass {
 	public static void main(String[] args) throws java.lang.Exception {
 		Solution instance = new Solution();
 	}
 }
 class Solution {
+	Solution() {
+		// For codechef check (!= null) condition
+		if(System.getProperty("ONLINE_JUDGE") == null) fileIO();
+		else consoleIO();
+
+		int t = 1;
+		// t = scn.nextInt(); 
+
+		this.preProcess();
+		for(int testCase = 1 ; testCase <= t ; testCase++) {
+			this.getMeTheSolution(testCase);
+		}
+
+		if(local == true) {
+			endTime = System.currentTimeMillis();
+			cerr.println("[Finished in : " + (endTime - startTime) + " ms]");
+			cerr.flush();
+			cerr.close();
+		}
+		pw.flush();
+		pw.close();
+	}
+	// Dont use Arrays.sort() as it used quick sort in the worst case (O(N ^ 2) running time which will give TLE)
+	/*****************************************************************************************
+
+
+	*/
+	
+
+	private void getMeTheSolution(int testCase) {
+		
+	}
+
+	private void preProcess() {}
+
+	/****************************************************************************************/
 	private void lend() {pw.println();}
 	private void YES() {pw.println("YES");}
 	private void NO() {pw.println("NO");}
@@ -15,14 +52,14 @@ class Solution {
 	private void yes() {pw.println("yes");}
 	private void no() {pw.println("no");}
 	private void printArray(int arr[]) {for(int i : arr) pw.print(i + " ");lend();}
-	private int _gcd(int a , int b) {return b == 0 ? a : _gcd(b , a % b);}
-	private int _lcm(int a , int b) {int hcf = _gcd(a , b);return (a * b) / hcf;}
+	private int HCF(int a , int b) {return b == 0 ? a : HCF(b , a % b);}
+	private int LCM(int a , int b) {int hcf = HCF(a , b);return (a * b) / hcf;}
 	private int max(int a , int b) {return (a > b) ? a : b;}
 	private int min(int a , int b) {return (a < b) ? a : b;}
 	private int accumulate(int arr[]) {int sum = 0;for(int i : arr) sum += i; return sum;}
 	private final long mod = 1000000007;
-	private final int INT_MAX = Integer.MAX_VALUE;
-	private final int INT_MIN = Integer.MIN_VALUE;
+	private final int inf = Integer.MAX_VALUE;
+	private final int neginf = Integer.MIN_VALUE;
 	private FastReader scn;
 	private PrintWriter pw;
 	private PrintWriter cerr;
@@ -46,50 +83,13 @@ class Solution {
 	}
 	private void reverse(int arr[]) {
 		int start = 0 , end = arr.length - 1;
-		while(start <= end) {
+		while (start <= end) {
 			int tmp = arr[start];
 			arr[start] = arr[end];
 			arr[end] = tmp;
 			start++;
 			end--;
 		}
-	}
-
-	/****************************************************************************************/
-	Solution() {
-		if(System.getProperty("ONLINE_JUDGE") == null) fileIO();
-		else consoleIO();
-
-		int t = 1;
-		// t = scn.nextInt();
-
-		this.preProcess();
-		for(int testCase = 1 ; testCase <= t ; testCase++) {
-			this.getMeTheSolution();
-		}
-
-		if(local == true) {
-			endTime = System.currentTimeMillis();
-			cerr.println("[Finished in : " + (endTime - startTime) + " ms]");
-			cerr.flush();
-			cerr.close();
-		}
-		pw.flush();
-		pw.close();
-	}
-  /*
-  
-  */
-	/****************************************************************************************/
-  
-
-	private void getMeTheSolution() {
-
-	}
-
-
-	private void preProcess() {
-		
 	}
 
 
@@ -130,29 +130,28 @@ class Solution {
 		}
 		int[] readIntArray(int n) {
 			int arr[] = new int[n];
-			for(int i = 0 ; i < n ; i++) arr[i] = nextInt();
+			for (int i = 0 ; i < n ; i++) arr[i] = nextInt();
 			return arr;
 		}	
 		long[] readLongArray(int n) {
 			long arr[] = new long[n];
-			for(int i = 0 ; i < n ; i++) arr[i] = nextLong();
+			for (int i = 0 ; i < n ; i++) arr[i] = nextLong();
 			return arr;
 		}
 		double[] readDoubleArray(int n) {
 			double arr[] = new double[n];
-			for(int i = 0 ; i < n ; i++) arr[i] = nextDouble();
+			for (int i = 0 ; i < n ; i++) arr[i] = nextDouble();
 			return arr;			
 		}
 		String[] readStringArray(int n) {
 			String arr[] = new String[n];
-			for(int i = 0 ; i < n ; i++) arr[i] = next();
+			for (int i = 0 ; i < n ; i++) arr[i] = next();
 			return arr;
 		}
 		char[] readCharArray(int n) {
 			char arr[] = new char[n];
-			for(int i = 0 ; i < n ; i++) arr[i] = nextChar();
+			for (int i = 0 ; i < n ; i++) arr[i] = nextChar();
 			return arr;
 		}
 	}
-
 }

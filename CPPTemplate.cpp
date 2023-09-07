5903d233d3e4e37a0a416ed1e746d9f087c7bf9b
Badal Soni <111721917+BadalSoni03@users.noreply.github.com>
2023-09-07 10:20:45 +0530

    Update CPPTemplate.cpp

--- CPPTemplate.cpp
+++ CPPTemplate.cpp
@@ -73,7 +73,7 @@ namespace MathFunc {
     double min(vector<double> ele) {return *min_element(all(ele));}
     ll HCF(ll a , ll b) {return !b ? a : HCF(b , a % b);}
     ll LCM(ll a , ll b) {ll hcf = HCF(a , b); return (a * b) / hcf;}
-    ll stoLL(string str) {ll num = 0; for (auto & it : str) num *= 10 + it; return num;}
+    ll stoLL(string str) {ll num = 0; for (auto & it : str) num *= 10 + (it - '0'); return num;}
     ll add(ll a , ll b) {return ((a % MOD) + (b % MOD)) % MOD;}
     ll sub(ll a , ll b) {return ((a % MOD) - (b % MOD) + MOD) % MOD;}
     ll mul(ll a , ll b) {return ((a % MOD) * (b % MOD)) % MOD;}
